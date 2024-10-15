587d1cc0c0c7120a7fa47e634c1ce6b18f0ecc7e
hamza123-oop <muhammadhamza01101@gmail.com>
2024-10-15 12:08:31 -0700

    Add files via upload

---
+++ Ass Program 1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+class studen
+{
+	private:
+		int roll_no;
+		char name [15];
+		public:
+			void input ()
+{
+	cout<<"Enter Roll Number of Student:";
+	cin>>roll_no;
+	cout<<"Enter Student Name:";
+	cin>>name;
+}
+void print (void)
+{
+	cout<<"Roll Number:"<<roll_no<<endl;
+	cout<<"Name:"<<name<<endl;
+}
+ void input ()
+ {
+ 	cout<<"Enter Roll Number of Students:";
+ 	cin>>roll_no;
+ 	cout<<"Enter Student Name:";
+ 	cin>>name;
+ }
+ void print (void)
+ {
+ 	cout<<"Roll Number:"<<roll_no<<endl;
+ 	cout<<"Name:"<<name<<endl;
+ }
+ int main ()
+ Student.Student;
+ Student.Input();
+ Student.Print;()
+ return 0;
+}
